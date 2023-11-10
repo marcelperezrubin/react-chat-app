cb58abcd2eac3cc0c15d423e9c3f9ff750978cf9
Marcel Rubin <marcelgabber89@gmail.com>
2023-11-10 17:42:17 +0100

    first commit capstone project

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import { Socket } from 'dgram';
+import express from 'express';
+import http from 'http';
+import {Server as SocketServer} from 'socket.io';
+
+const app = express()
+const server = http.createServer(app)
+const io = new SocketServer(server)
+
+io.on('connect', socket => {
+    console.log('Client connected')
+})
+
+server.listen(3000)
+console.log('Server on port', 3000)
