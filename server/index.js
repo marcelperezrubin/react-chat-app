05cc8eaa3024c0920bda36cf3b5db2bc033591b3
Marcel Rubin <marcelgabber89@gmail.com>
2023-11-10 18:10:44 +0100

    project configured 1 part

--- server/index.js
+++ server/index.js
@@ -1,15 +1,10 @@
-import { Socket } from 'dgram';
 import express from 'express';
 import http from 'http';
 import {Server as SocketServer} from 'socket.io';
 
 const app = express()
 const server = http.createServer(app)
-const io = new SocketServer(server, {
-    cors: {
-        origin: "http://localhost:5173"
-    }
-})
+const io = new SocketServer(server)
 
 io.on('connect', socket => {
     console.log('Client connected')
