4e66a4397fa7d301ecfa7033b98e2b1ce9687289
Marcel Rubin <marcelgabber89@gmail.com>
2023-11-10 18:19:08 +0100

    Update values

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,31 @@
-import io from 'socket.io-client'
+import io from 'socket.io-client';
+import { useState } from 'react';
 
 const socket = io("/")
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(message);
+  };
+
   return(
-    <div>hello world</div>
-  )
+    <div>
+
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder='Write your message...' 
+      onChange={(e) => setMessage(e.target.value)}
+      
+      />
+      <button>
+        Send
+      </button>
+    </form>
+
+    </div>
+  );
 }
 
 export default App
