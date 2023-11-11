1bdd91436b1db5a9841ad6404303c75255dd048f
Marcel Rubin <marcelgabber89@gmail.com>
2023-11-11 19:12:59 +0100

    update react-dom

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['react/jsx-runtime'],
+      external: ['react', 'react-dom'],
     },
   },
   base: '/react-chat-app/frontend/dist/',
