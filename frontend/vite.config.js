27920fb4bd7b311d10015d27027607d8fdc4302d
Marcel Rubin <marcelgabber89@gmail.com>
2023-11-11 19:28:51 +0100

    frontend update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
-// frontend/vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,12 +10,6 @@ export default defineConfig({
         target: 'ws://localhost:3000',
         ws: true,
       },
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-    },
-  },
-  base: '/react-chat-app/frontend/dist/',
-});
+    }
+  }
+})
