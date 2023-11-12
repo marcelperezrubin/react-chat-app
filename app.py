b0adabeba8775715ff31826ac2e57cf6e1b903cd
Marcel Rubin <marcelgabber89@gmail.com>
2023-11-12 17:13:11 +0100

    update values

--- app.py
+++ app.py
@@ -6,12 +6,7 @@ import requests
 
 app = Flask(__name__, static_folder='frontend/dist')
 CORS(app, resources={r"/*": {"origins": "https://react-chat-capstone-project-5721959f643e.herokuapp.com"}})
-socketio = SocketIO(
-    app,
-    cors_allowed_origins="http://localhost:5173",
-    async_mode='eventlet',  # O 'threading' o 'gevent'
-    transports=['websocket', 'polling'],
-)
+socketio = SocketIO()
 
 # Mantén un seguimiento de los nombres de usuario asignados
 user_count = 0
